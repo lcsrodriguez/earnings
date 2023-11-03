1ce8e487e7881d05e0a78d01f2e8c13fb221494c
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-03 22:04:28 +0100

    Adding symbol/ticker check + post func

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+from .constants import *
+from typing import Union
+import requests
+
+
+def post(url: str, p) -> requests.Response:
+    r = requests.post(url=url, headers={
+        "Referer": "https://www.earningswhispers.com"
+    }, data=p)
+    return r
+
+
+def checkSymbol(_sym: str = "") -> bool:
+    try:
+        r = post("https://www.earningswhispers.com/api/tickers", {"symbol": _sym})
+    except Exception as e:
+        print(f"Exception (error: {e})")
+        return False
+    if r.status_code == 200: # // 100 == 2:
+        return len([k for k in r.json() if k["ticker"] == str(_sym).upper()]) == 1
+    return False
+
