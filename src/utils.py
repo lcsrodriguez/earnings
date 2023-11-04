aba5a5e3cf875fdc58acaaccd90933628b7946e2
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-04 12:29:29 +0100

    Updating URLs using cstes

--- src/utils.py
+++ src/utils.py
@@ -5,21 +5,21 @@ import requests
 
 def get(url: str = "") -> requests.Response: # Union[dict, str]:
     r = requests.get(url=url, headers={
-        "Referer": "https://www.earningswhispers.com"
+        "Referer": f"{MAIN_URL}"
     })
     return r
 
 
 def post(url: str, p) -> requests.Response:
     r = requests.post(url=url, headers={
-        "Referer": "https://www.earningswhispers.com"
+        "Referer": f"{MAIN_URL}"
     }, data=p)
     return r
 
 
 def checkSymbol(_sym: str = "") -> bool:
     try:
-        r = post("https://www.earningswhispers.com/api/tickers", {"symbol": _sym})
+        r = post(f"{MAIN_URL}/api/tickers", {"symbol": _sym})
     except Exception as e:
         print(f"Exception (error: {e})")
         return False
