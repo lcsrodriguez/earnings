c630fc841e2b5b93e85816e0afdff58d8f3e6380
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-19 21:51:59 +0100

    Adding getExpectedPriceAction and getExpectedPriceAction

--- earnings/main.py
+++ earnings/main.py
@@ -4,10 +4,11 @@ from .utils import *
 ## TODO: Calendar (with earnings calendar)
 
 class Earnings:
-    __slots__ = ("_sym",)
+    __slots__ = ("_sym", "stockData")
 
     def __init__(self, sym: str = ""):
         self._sym = str(sym).upper()
+        self.stockData: Union[dict, None] = None
 
     def _premCheck(self) -> bool:
         return checkSymbol(self._sym)
@@ -19,9 +20,10 @@ class Earnings:
         print(self.__str__())
 
     def getCompanyInfo(self) -> dict:
-        ...
-
-    # 1: Expect -- 2: Vote
+        if self.stockData is None:
+            r = get(f"{MAIN_URL}/api/getstocksdata/{self._sym}")
+            self.stockData = r.json()[self._sym]
+        return self.stockData
 
     def getExpectationsEstimates(self):
         ...
@@ -29,3 +31,22 @@ class Earnings:
     def getQuotes(self):
         r = get(f"{MAIN_URL}/api/getquotes/{self._sym}")
         return r.json()[self._sym]
+
+    def getExpectedPriceAction(self):
+        r = post(f"{MAIN_URL}/api/vote",
+                 {
+                     "ticker": self._sym,
+                     "vote": DEFAULT_POST_OPTIONS["vote"]
+                 })
+        r.json()[-1]["totalHolds"] -= 1
+        return r.json()
+
+    def getExpectedPriceAction(self):
+        r = post(f"{MAIN_URL}/api/expect",
+                 {
+                     "ticker": self._sym,
+                     "vote": DEFAULT_POST_OPTIONS["expect"]
+                 })
+        r.json()[-1]["meet"] -= 1
+        r.json()[-1]["total"] -= 1
+        return r.json()
