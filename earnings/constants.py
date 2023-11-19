22dd7bff2868d5adf5fcab53d9e9b3e23c55a0c3
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-19 23:00:39 +0100

    Adding Frequency

--- earnings/constants.py
+++ earnings/constants.py
@@ -1,2 +1,8 @@
+from enum import Enum
 MAIN_URL: str = "https://www.earningswhispers.com"
 DEFAULT_POST_OPTIONS: dict = {"expect": "Meet", "vote": "flat"}
+
+
+class Frequency(Enum):
+    DAILY: str = "daily"
+    WEEKLY: str = "weekly"
