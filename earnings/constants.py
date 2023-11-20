5dc8b8e87ad8e18388b4a849bbe74d18162ac590
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-20 23:19:27 +0100

    Adding sectors enum

--- earnings/constants.py
+++ earnings/constants.py
@@ -6,3 +6,17 @@ DEFAULT_POST_OPTIONS: dict = {"expect": "Meet", "vote": "flat"}
 class Frequency(Enum):
     DAILY: str = "daily"
     WEEKLY: str = "weekly"
+
+
+class Sector(Enum):
+    XLB: str = "Materials"
+    XLC: str = "Communication Services"
+    XLE: str = "Energy"
+    XLF: str = "Financials"
+    XLI: str = "Industrials"
+    XLK: str = "Technology"
+    XLP: str = "Consumer Staples"
+    XLRE: str = "Real Estate"
+    XLU: str = "Utilities"
+    XLV: str = "Health Care"
+    XLY: str = "Consumer Discretionary"
