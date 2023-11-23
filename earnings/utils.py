71f885ba01052dbe9dbbd1585ea36532c3a31ace
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-11-23 20:03:04 +0100

    Refactoring utils

--- earnings/utils.py
+++ earnings/utils.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 tdf: Callable[[dict], pd.DataFrame] = lambda j: pd.DataFrame(data=j)
 
+gd: Callable[[Union[set, list]], dict] = lambda ks, d: {k: d[k] for k in list(ks) if k in d}
+
 
 def get(url: str = "") -> requests.Response: # Union[dict, str]:
     r = requests.get(url=url, headers={
@@ -14,7 +16,7 @@ def get(url: str = "") -> requests.Response: # Union[dict, str]:
     return r
 
 
-def post(url: str, p) -> requests.Response:
+def post(url: str = "", p: Union[dict, None] = None) -> requests.Response:
     r = requests.post(url=url, headers={
         "Referer": f"{MAIN_URL}"
     }, data=p)
