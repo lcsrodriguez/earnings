50de8dec4f41aedb1ccbfe5662a973dc9c2b32f4
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-02 22:26:56 +0100

    Adding fields to setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 import setuptools
+from earnings import __version__
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="earnings",
+    version=__version__,
+    author="Lucas RODRIGUEZ",
+)
