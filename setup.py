d223790231e6b72d517abbcef69ad6801af432fb
lcsrodriguez <lucasrodriguez.08@outlook.com>
2023-12-02 22:28:50 +0100

    Adding fields to setup.py 2

--- setup.py
+++ setup.py
@@ -8,4 +8,14 @@ setuptools.setup(
     name="earnings",
     version=__version__,
     author="Lucas RODRIGUEZ",
+    author_email="lcsrodriguez@pm.me",
+    description=" Equity earnings Python package (confirmed calendar, news articles, earnings transcripts, ...)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
